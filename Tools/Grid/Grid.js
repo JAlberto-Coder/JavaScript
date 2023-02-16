ac08fd46d2c1d62dfbd53fd6f361dffd4140f8aa
Jorge Alberto <51796956+JAlberto-Coder@users.noreply.github.com>
2023-02-16 12:36:34 -0600

    Se mejora la sixtaxis, para un mejor performance

--- Tools/Grid/Grid.js
+++ Tools/Grid/Grid.js
@@ -1,168 +1,164 @@
+"use strict";
 // =======================================================
 // JALberto-Coder | Se crea una tabla con estilos de bootrap
 // =======================================================
-var objGrid = {
+const objGrid = {
 	Construir: function (idGrid, columnas, titulo = "", filasPorPagina = 15, altura = "", color = "#330073") {
 		try {
-			var grid = document.getElementById(idGrid);
-			grid.classList.add("rounded");
-			grid.classList.add("bg-white");
+			// Verificar que los argumentos sean válidos
+			if (!idGrid || !Array.isArray(columnas) || columnas.length === 0) {
+				throw new Error("Los argumentos son inválidos");
+			}
+			// Crear la tabla
+			const grid = document.getElementById(idGrid);
+			grid.classList.add("rounded", "bg-white");
 			grid.style.width = "100%";
 			grid.style.borderRadius = "5px";
-			grid.innerHTML = "<div id='" + idGrid + "-contenedor'></div>";
-	
-			var contenedor = document.getElementById(idGrid + "-contenedor");
-			contenedor.style.display = "block";
-			contenedor.style.width = "100%";
-			contenedor.style.overflowX = "auto";
-			contenedor.innerHTML = "<div id='" + idGrid + "-contenedor-table'></div>";
-	
-			var contenedorTable = document.getElementById(idGrid + "-contenedor-table");
-			contenedorTable.classList.add("table-responsive");
-			contenedorTable.style.borderCollapse = "separate";
-			contenedorTable.style.width = "100%";
-	
+			grid.innerHTML = `<div id="${idGrid}-contenedor"></div>`;
+
+			const gridContainer = document.getElementById(`${idGrid}-contenedor`);
+			gridContainer.style.display = "block";
+			gridContainer.style.width = "100%";
+			gridContainer.style.overflowX = "auto";
+			gridContainer.innerHTML = `<div id="${idGrid}-contenedor-table"></div>`;
+
+			const tableContainer = document.getElementById(`${idGrid}-contenedor-table`);
+			gridContainer.classList.add("table-responsive");
+			gridContainer.style.borderCollapse = "separate";
+			gridContainer.style.width = "100%";
+			
 			if (titulo.length > 0) {
-				contenedorTable.innerHTML = "<h4 style='margin:5px;'>" + titulo + "</h4>";
+				tableContainer.innerHTML = `<h4 style="margin:5px;">${titulo}</h4>`;
 			}
-	
-			var tabla = document.createElement("table");
-			tabla.id = "tbl" + idGrid;
-			tabla.classList.add("table-hover");
-			tabla.classList.add("table-ecore");
-			tabla.style.width = "100%";
-			contenedorTable.append(tabla);
-			var tablaEncabezado = document.createElement("thead");
-			var filaEncabezado = document.createElement("tr");
-			tablaEncabezado.append(filaEncabezado);
-	
-			var tamanio = columnas.length;
-			for (var i = 0; i < tamanio; i++) {
-				var tamanioAncho = parseFloat(Math.round((100 / tamanio) * Math.pow(10, 2)) / Math.pow(10, 2));
-				var ancho = (columnas[i].width === undefined) || columnas[i].width === null || columnas[i].width === "" ? tamanioAncho + "%" : columnas[i].width;
-				var columna = document.createElement("td");
-				columna.id = "row" + columnas[i].field;
-				columna.scope = "col";
-				columna.classList.add("font-weight-bold");
-				columna.classList.add("text-white");
-				columna.classList.add("h5");
-				columna.classList.add("text-" + columnas[i].align);
-				columna.style.backgroundColor = color;
-				columna.width = ancho;
-				columna.innerText = columnas[i].title;
-				filaEncabezado.append(columna);
-			}
-			tabla.append(tablaEncabezado);
-	
-			var tablaCuerpo = document.createElement("tbody");
-			tablaCuerpo.id = "tbl" + idGrid + "-body";
-			tablaCuerpo.style.height = altura.length === 0 ? "100%" : altura;
-			tabla.append(tablaCuerpo);
-	
-			var tablaPie = document.createElement("tfoot");
-			var filaPie = document.createElement("tr");
-			tablaPie.append(filaPie);
-			tabla.append(tablaPie);
-	
-			var columnaPie = document.createElement("td");
-			columnaPie.classList.add("td-color-morado");
-			columnaPie.classList.add("td-footer");
-			columnaPie.classList.add("h5");
-			columnaPie.colspan = tamanio;
-	
-			filaPie.append(columnaPie);
-			var smallPaginacion = document.createElement("small");
-			smallPaginacion.id = "tbl" + idGrid + "-paginacion";
+			
+			const table = document.createElement("table");
+			table.id = `tbl${idGrid}`;
+			table.classList.add("table-hover", "table-ecore");
+			table.style.width = "100%";
+			gridContainer.append(table);
+
+			const tableHead = document.createElement("thead");
+			const headRow = document.createElement("tr");
+			tableHead.append(headRow);
+
+			const tamanio = columnas.length;
+			const columnCount = columnas;
+			columnCount.forEach(function (columna) {
+				const tamanioAncho = parseFloat(Math.round((100 / tamanio) * Math.pow(10, 2)) / Math.pow(10, 2));
+				const ancho = (columna.width === undefined) || columna.width === null || columna.width === "" ? tamanioAncho + "%" : columna.width;
+
+				const elementColum = document.createElement("td");
+				elementColum.id = `row${columna.field}`;
+				elementColum.scope = "col";
+				elementColum.classList.add("font-weight-bold", "text-white", "h5", `text-${columna.align}`);
+				elementColum.style.backgroundColor = color;
+				elementColum.style.width = ancho;
+				elementColum.innerText = columna.title;
+
+				headRow.append(elementColum);
+			});
+
+			tableHead.append(headRow);
+			table.append(tableHead);
+
+			const tableBody = document.createElement("tbody");
+			tableBody.id = `tbl${idGrid}-body`;
+			tableBody.style.height = altura.length === 0 ? "100%" : altura;
+			table.append(tableBody);
+
+			const tableFooter = document.createElement("tfoot");
+			const rowFooter = document.createElement("tr");
+			tableFooter.append(rowFooter);
+			table.append(tableFooter);
+
+			const columnFooter = document.createElement("td");
+			columnFooter.classList.add("td-color-morado", "td-footer", "h5");
+			columnFooter.colspan = tamanio;
+			rowFooter.append(columnFooter);
+
+			const smallPaginacion = document.createElement("small");
+			smallPaginacion.id = `tbl${idGrid}-paginacion`;
 			smallPaginacion.style.width = "50%";
 			smallPaginacion.classList.add("text-left");
-			columnaPie.append(smallPaginacion);
-	
-			var aLink = document.createElement("a");
+			columnFooter.append(smallPaginacion);
+
+			const aLink = document.createElement("a");
 			aLink.href = "#";
-			aLink.classList.add("a-lectura");
-			aLink.classList.add("table-paginacion");
+			aLink.classList.add("a-lectura", "table-paginacion");
 			aLink.innerText = "1";
 			smallPaginacion.append(aLink);
-	
-			var smallPaginas = document.createElement("small");
+
+			const smallPaginas = document.createElement("small");
 			smallPaginas.style.width = "50%";
-			smallPaginas.id = "tbl" + idGrid + "-paginas";
+			smallPaginas.id = `tbl${idGrid}-paginas`;
 			smallPaginas.innerText = "0 registros.";
-			smallPaginas.classList.add("text-right");
-			smallPaginas.classList.add("pull-right");
-			columnaPie.append(smallPaginas);
-		} catch(e) { 
-			alert(e);
+			smallPaginas.classList.add("text-right", "pull-right");
+			columnFooter.append(smallPaginas);
+		} catch (e) {
+			console.error(e);
 		}
 	},
-	Agregar: function (idGrid, columnas, titulo = "", filasPorPagina = 15, altura = "", color = "#330073") {
+	Agregar: function (idGrid, columnas, objRow) {
 		try {
-			var keys = Object.keys(objRow)
-				, values = Object.values(objRow);
+			const tablaBody = document.getElementById(`tbl${idGrid}-body`);
 
-			var tablaBody = document.getElementById("tbl" + idGrid + "-body");
+			const tamanio = columnas.length;
+			let filaHTML = '<tr id="${objGrid.GuidV4Generar()}" style="height: 18px; border-left: 1px solid #ccc; cursor: pointer;">';
 
-			var fila = document.createElement("tr");
-			fila.id = objGrid.GuidGenera();
-			fila.style.height = "18px";
-			fila.style.borderLeft = "1px solid #ccc";
-			fila.style.cursor = "pointer";
-			tablaBody.append(fila);
-
-			var color = "#330073";
-			var tamanio = columnas.length;
-			for (var i = 0; i < tamanio; i++) {
-				var tamanioAncho = parseFloat(Math.round((100 / tamanio) * Math.pow(10, 2)) / Math.pow(10, 2));
-				var ancho = !IsNotNullOrUndefined(columnas[i].width) || columnas[i].width === "" ? tamanioAncho + "%" : columnas[i].width;
-				var valor = IsNotNullOrUndefined(objRow[columnas[i].field]) ? objRow[columnas[i].field] : "";
-
-				var columna = document.createElement("td");
-				columna.scope = "col";
-				columna.classList.add("h5");
-				columna.width = ancho;
-				columna.innerText = valor;
-				fila.append(columna);
+			for (let i = 0; i < tamanio; i++) {
+				const tamanioAncho = parseFloat(Math.round((100 / tamanio) * Math.pow(10, 2)) / Math.pow(10, 2));
+				const ancho = !objGrid.Utilerias.EsIndefinidoONulo(columnas[i].width) || columnas[i].width !== "" ? `${tamanioAncho}%` : columnas[i].width;
+				const valor = objGrid.Utilerias.EsIndefinidoONulo(objRow[columnas[i].field]) ? objRow[columnas[i].field] : "";
+				filaHTML += `<td scope="col" class="h5" width="${ancho}">${valor}</td>`;
 			}
-			$("#tbl" + idGrid + "-body tr").unbind();
-			$("#tbl" + idGrid + "-body tr").click(function (e) {
-				$("#" + e.currentTarget.parentElement.id + " tr").removeClass("table-active");
 
-				if (!$("#" + e.currentTarget.id).hasClass("table-active"))
-					$("#" + e.currentTarget.id).addClass("table-active");
+			filaHTML += '</tr>';
+			tablaBody.insertAdjacentHTML('beforeend', filaHTML);
+
+			$(`#tbl${idGrid}-body tr`).unbind().click(function (e) {
+				$(`#${e.currentTarget.parentElement.id} tr`).removeClass("table-active");
+
+				if (!$(`#${e.currentTarget.id}`).hasClass("table-active")) {
+					$(`#${e.currentTarget.id}`).addClass("table-active");
+				}
 			});
 		} catch (e) {
-			alert(e);
+			console.error(e);
 		}
 	},
 	Eliminar: function (idGrid) {
 		try {
-			var elementos = document.querySelectorAll(idGrid + " tr.table-active");
+			const registrosSeleccionados = document.querySelectorAll(`${idGrid} tr.table-active`);
 
-			if (elementos.length < 1) {
-				alert("Debes de seleccionar un registro para poder realizar esta acción.");
+			if (registrosSeleccionados.length < 1) {
+				const table = document.querySelector(idGrid);
+				table.classList.add("seleccion-requerida");
 				return;
 			}
-			var elemento = document.getElementById(elementos[0].id);
-			elemento.remove();
+
+			const registroSeleccionado = document.getElementById(registrosSeleccionados[0].id);
+			registroSeleccionado.remove();
 		} catch (e) {
-			console.log(e);
+			console.error(e);
 		}
 	},
 	Utilerias: {
-		GuidGenera: function () {
-			var d = new Date().getTime();
-			var d2 = ((typeof performance !== 'undefined') && performance.now && (performance.now() * 1000)) || 0;
-			return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
-				var r = Math.random() * 16;
-				if (d > 0) {
-					r = (d + r) % 16 | 0;
-					d = Math.floor(d / 16);
-				} else {
-					r = (d2 + r) % 16 | 0;
-					d2 = Math.floor(d2 / 16);
-				}
-				return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+		GuidV4Generar: function () {
+			const timestamp = new Date().getTime();
+			const [performanceNow = 0] = performance ? [performance.now() * 1000] : [];
+			const UUID_PATTERN = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx';
+
+			return UUID_PATTERN.replace(/[xy]/g, function (c) {
+				const r = Math.random() * 16;
+				const randomNumber = (timestamp > 0)
+					? (timestamp + r) % 16 | 0
+					: (performanceNow + r) % 16 | 0;
+				const uuidDigit = (c === 'x') ? randomNumber : (randomNumber & 0x3 | 0x8);
+				return uuidDigit.toString(16);
 			});
+		},
+		EsIndefinidoONulo: function (valor) {
+			return valor !== null && valor !== undefined;
 		}
 	}
 };
